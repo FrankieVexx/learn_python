411a6e37dd92e29b26123909d5bc3e997eae9086
Frankie Vexx <franblog@gmail.com>
2024-05-17 09:22:07 +0300

    update multiple assignment

--- pythonn/Main/Multiplesassignment.py
+++ pythonn/Main/Multiplesassignment.py
@@ -2,16 +2,16 @@
 #age = 25
 #attractive = True
 
-#name, age, attractive = "Francis", 25, True
+name, age, attractive = "Francis", 25, True
 
-#print(name)
-#print(age)
-#print(attractive)
+print(name)
+print(age)
+print(attractive)
 
-#james = 27
-#susan = 27
-#Josh = 27
-#Sam = 27
+james = 27
+susan = 27
+Josh = 27
+Sam = 27
 
 james = susan = Josh = Sam = 27
 
--- pythonn/Main/loopcontrolstatements.py
+++ pythonn/Main/loopcontrolstatements.py
@@ -3,21 +3,21 @@
 # continue = skips to the next iteration of the loop
 # pass = does nothing, acts as a place holder
 
-#while True:
-   #name = input("Enter your name: ")
-   #if name != "":
-       #break
+while True:
+   name = input("Enter your name: ")
+   if name != "":
+       break
 
-#continue
+continue
 
-#phone_number = "123-456-7890"
+phone_number = "123-456-7890"
 
-#for i in phone_number:
-    #if i == "-":
-        #continue
-    #print(i, end="")
+for i in phone_number:
+    if i == "-":
+        continue
+    print(i, end="")
 
-#
+
 for i in range(1, 21):
     if i == 13:
         pass
